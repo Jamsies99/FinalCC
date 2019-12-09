32ba3dab8d018c3a15a817b23cbbbba2d9589ab5
Jamsies99 <jamsies99@gmail.com>
2019-12-09 14:38:13 -0500

    Add files via upload

---
+++ Arduino/gamecontroller.ino
@@ -0,0 +1,32 @@
+
+const int buttonPin = 3; 
+const int buttonTwoPin = 6; 
+int knob = 0;
+int val;
+int pos; 
+//int positions = [];
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonTwoPin, INPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  val = analogRead(knob); 
+  int switchState = digitalRead(buttonPin);
+  int switchStateTwo = digitalRead(buttonTwoPin); 
+  // put your main code here, to run repeatedly:
+ // positions = pos;
+  
+  //Serial.write(pos);
+  //Serial.write(pos);
+  //Serial.write(switchState);
+  //Serial.write(switchStateTwo);
+  Serial.write(val);
+  //Serial.print(sensorValue);
+//  Serial.println("button: ");
+//  Serial.print(switchState);
+//  Serial.println("button 2: ");
+//  Serial.print(switchStateTwo);
+}
